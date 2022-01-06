dd6f42912420ac7becaaca6488ccce0d037ee14f
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2022-01-06 16:25:24 -0300

    change link orders and fix icon width

--- src/layouts/BaseResponsiveLayout/quickNavigationMenus.ts
+++ src/layouts/BaseResponsiveLayout/quickNavigationMenus.ts
@@ -1,7 +1,7 @@
 export default [
-  { label: 'Regions', link: '/home', icon: 'fas fa-globe' },
-  { label: 'Pokedexes', link: '/home', icon: 'fas fa-tablet-alt' },
-  { label: 'Pokemons', link: '/home', icon: 'fas fa-dragon' },
+  { label: 'Pokemons', link: '/home', icon: 'fas fa-dragon fa-fw' },
+  { label: 'Pokedexes', link: '/home', icon: 'fas fa-tablet-alt fa-fw' },
+  { label: 'Regions', link: '/home', icon: 'fas fa-globe fa-fw' },
 ]
 
 export type QuickNavigationInfoType = {
