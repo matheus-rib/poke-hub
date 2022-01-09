e182fc4baa34f0aa2a2040563d793e6f42b29f01
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2022-01-09 00:22:45 -0300

    Mobile layout for pokemons page

--- src/pages/Pokemons/components/Header/mobile/index.tsx
+++ src/pages/Pokemons/components/Header/mobile/index.tsx
@@ -1,5 +1,19 @@
+import { Flex, Heading } from '@chakra-ui/react'
 import React from 'react'
+import Search from '../../../../../components/Search'
 
 export default function Mobile() {
-  return <div></div>
+  return (
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      mb="5"
+    >
+      <Heading fontSize="4xl" mb="2">
+        Pokémons
+      </Heading>
+      <Search type="pokemon" />
+    </Flex>
+  )
 }
