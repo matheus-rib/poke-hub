a0a8ba725db863dca6f7d660d2498c753abf3bbd
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2022-01-08 21:14:56 -0300

    layout fix

--- src/pages/Pokemons/index.tsx
+++ src/pages/Pokemons/index.tsx
@@ -22,7 +22,7 @@ export default function Pokemons() {
   return (
     <Container>
       <Header />
-      <SimpleGrid columns={[2, 3, 5]} spacing={5} padding="1">
+      <SimpleGrid columns={[2, 3, 5]} spacing={5} padding="1" mt="2">
         {pokemons?.map(pokemon => (
           <GridItem key={pokemon.name} height="150px">
             <Card id={pokemon.id} name={pokemon.name} sprite={pokemon.sprite} />
