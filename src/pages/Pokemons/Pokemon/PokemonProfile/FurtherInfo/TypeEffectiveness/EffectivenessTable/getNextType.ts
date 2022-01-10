aba9874655e551c4797df6776f478a2355f6e655
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2022-01-09 23:40:18 -0300

    create generator to make an easier interation to effectiveness table

---
+++ src/pages/Pokemons/Pokemon/PokemonProfile/FurtherInfo/TypeEffectiveness/EffectivenessTable/getNextType.ts
@@ -0,0 +1,18 @@
+import {
+  DamageMultiplier,
+  PokemonType,
+} from '../../../../../../../utils/typeEffectiveness/types'
+
+type NextTypeEffectivenessParam = Array<{
+  types: Array<PokemonType | never>
+  effectiveness: DamageMultiplier
+}>
+
+export default function* (typesAndEffectiveness: NextTypeEffectivenessParam) {
+  for (const typeAndEffectiveness of typesAndEffectiveness) {
+    const effectiveness = typeAndEffectiveness.effectiveness
+    for (const type of typeAndEffectiveness.types) {
+      yield { type, effectiveness }
+    }
+  }
+}
